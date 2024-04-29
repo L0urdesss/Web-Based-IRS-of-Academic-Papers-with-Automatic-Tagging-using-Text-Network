b65bf8e9c83f626714441911667a918361017463
Louuu <villaruzmarialourdes21@gmail.com>
2024-04-29 19:41:15 +0800

    First Commit

--- resources/js/Pages/Papers/User/Partials/PaperDetails.jsx
+++ resources/js/Pages/Papers/User/Partials/PaperDetails.jsx
@@ -1,7 +1,9 @@
+// PaperDetails.jsx
+import { useState, useEffect, useRef } from 'react';
 import InputLabel from '@/Components/InputLabel';
 import TextInput from '@/Components/TextInput';
 import { useForm } from '@inertiajs/react';
-import { useEffect, useRef, useState } from 'react';
+import RequestForm from '@/Components/RequestForm'; // Adjust the path as needed
 
 // Import the request_button.png, pdf_icon.png, and x_button.png images
 import requestButtonImg from '@/Components/request_button.png';
@@ -17,9 +19,7 @@ export default function PaperDetails({ user, paper, className = '', success }) {
     });
 
     const { name, email } = user;
-    console.log(user)
-    console.log(paper)
-
+    const [course, setCourse] = useState('');
     const { title, date_published, author, abstract } = paper;
     const [showForm, setShowForm] = useState(false);
 
@@ -47,7 +47,6 @@ export default function PaperDetails({ user, paper, className = '', success }) {
 
     const handleRequestButtonClick = () => {
         setShowForm(true);
-        console.log("Shown")
     };
 
     return (
@@ -98,13 +97,11 @@ export default function PaperDetails({ user, paper, className = '', success }) {
                                 <p>No file</p>
                             )}
 
-                            {/* Text describing the file */}
                             <p className="text-sm mt-2 mb-4" style={{ fontSize: '10px', lineHeight: '1.4' }}>
                                 To read the full-text of this research,<br></br>
                                 you can request a copy directly<br></br> from the authors.
                             </p>
 
-                            {/* Replace the PDF icon with the request_button.png image */}
                             <img
                                 src={requestButtonImg}
                                 alt="Request Button"
@@ -121,76 +118,14 @@ export default function PaperDetails({ user, paper, className = '', success }) {
                 </div>
             </div>
             {showForm && (
-              <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center" style={{ backdropFilter: 'blur(2px)' }}>
-                   <div className="bg-white rounded-md relative" style={{ width: '350px', minHeight: '300px', maxHeight: '80vh', overflowY: 'auto', borderRadius: '10px' }}>
-                        <button className="absolute right-0 top-0 p-2" onClick={handleCloseForm}>
-                            {/* Replace the SVG with the image */}
-                            <img src={exitButtonImg} alt="Exit Button" className="h-3 w-3" />
-                        </button>
-                        {/* Background color with logo */}
-                        <div style={{ backgroundColor: '#831B1C', display: 'flex', alignItems: 'center', justifyContent: 'center', height: '35px' }}>
-                            <img src={logoImg} alt="Logo" className="block" style={{ width: 'auto', height: '200%' }} />
-                        </div>
-                        {/* Your form components here */}
-                        <form onSubmit={submit}>
-                            <div className="p-5"> {/* Add padding to the container */}
-                                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', fontSize: '16px', color: '#831B1C', fontWeight: 'bold' }}>
-                                    Submit Your Request Here!
-                                </div>
-                                <div style={{ fontSize: '10px', lineHeight: '1.4', color: '#831B1C', marginBottom: '15px' }}>
-                                    Fill out this form to send us your request. It's the fastest and easiest way to get in touch!
-                                </div>
-                                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: '10px', fontSize: '10px' }}>
-                                    <label htmlFor="name" style={{ marginRight: '5px', width: '50px', textAlign: 'left',fontWeight: 'bold' }}>Name:</label>
-                                    <input type="text" id="name" value={name} disabled className="rounded-md border-none bg-gray-200 p-1" style={{ width: 'calc(100% - 60px)', fontSize: '10px', borderRadius: '3px', }} />
-                                </div>
-                                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: '10px', fontSize: '10px' }}>
-                                    <label htmlFor="email" style={{ marginRight: '5px', width: '50px', textAlign: 'left' ,fontWeight: 'bold'}}>Email:</label>
-                                    <input type="email" id="email" value={email} disabled className="rounded-md border-none bg-gray-200 p-1" style={{ width: 'calc(100% - 60px)', fontSize: '10px', borderRadius: '3px' }} />
-                                </div>
-                                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: '10px', fontSize: '10px' }}>
-                                    <label htmlFor="title" style={{ marginRight: '5px', width: '50px', textAlign: 'left' ,fontWeight: 'bold'}}>Title:</label>
-                                    <input type="text" id="title" value={title} disabled className="rounded-md border-none bg-gray-200 p-1" style={{ width: 'calc(100% - 60px)', fontSize: '10px', borderRadius: '3px' }} />
-                                </div>
-                                <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '10px' }}>
-                                    <label htmlFor="purpose" style={{ fontSize: '14px' }}></label>
-                                    <textarea
-                                        id="purpose"
-                                        value={data.purpose}
-                                        onChange={(e) => setData('purpose', e.target.value)}
-                                        className="rounded-md border-none bg-gray-200 p-1 mt-1"
-                                        style={{ height: '100px', width: '100%', resize: 'none', fontSize: '10px', borderRadius: '3px' }}
-                                        placeholder="Describe Your Purpose here.."
-                                        onFocus={(e) => e.target.placeholder = ''}
-                                        onBlur={(e) => e.target.placeholder = 'Describe Your Purpose here..'}
-                                    />
-                                </div>
-                                {/* Other form fields */}
-                            </div>
-                            <button
-                                type="submit"
-                                style={{
-                                    backgroundColor: '#831B1C',
-                                    color: 'white',
-                                    borderRadius: '5px',
-                                    padding: '5px 5px',
-                                    fontSize: '12px',
-                                    marginTop: '5px', // Adjust as needed
-                                    width: '80%', // Adjust as needed
-                                    position: 'relative',
-                                    bottom:'20px', // Move the button up by 10px
-                                    marginLeft: 'auto',
-                                    marginRight: 'auto',
-                                    display: 'block',
-                                    fontWeight:'bold',
-                                }}
-                                onClick={submit}
-                            >
-                                Submit Form
-                            </button>
-                        </form>
-                    </div>
-                </div>
+                <RequestForm
+                    user={user}
+                    data={data}
+                    setData={setData}
+                    submit={submit}
+                    handleCloseForm={handleCloseForm}
+                    title={title}
+                />
             )}
         </section>
     );
